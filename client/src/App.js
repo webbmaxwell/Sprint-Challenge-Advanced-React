9955c0fd8f27e46757b3d951cd5447134942172b
webbmaxwell <webbwrites@gmail.com>
2020-04-05 14:16:49 -0600

    notes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import PlayerList from './Components/PlayerList.js';
@@ -7,19 +6,6 @@ import PlayerList from './Components/PlayerList.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Yes, this is hard, and yes, you should have prepared better, but...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          You can do this!
-        </a>
-      </header>
       <PlayerList />
     </div>
   );
